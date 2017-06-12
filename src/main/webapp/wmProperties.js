a5002265e9e1c3bfc441f6998af2f4f05e318ed8
root <root@9f2405e73829>
2017-06-12 10:19:19 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_xbeNeyRQZK",
+  "homePage" : "Main",
+  "name" : "Auto_xbeNeyRQZK",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
